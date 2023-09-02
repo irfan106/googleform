553ac3878432a36426a01c2d76d823d53439d779
IRFAN <86617983+irfan106@users.noreply.github.com>
2023-09-02 16:14:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const cors = require('cors'); // Import the 'cors' package
 
 const app = express();
 const port = 5000;
 
 app.use(bodyParser.json());
 
+// Use the 'cors' middleware to enable CORS for the '/api/submit' route
+app.use('/api/submit', cors({
+  origin: 'https://64f30e17cba86e678c9dc47b--celebrated-kelpie-300433.netlify.app',
+  methods: 'POST', // Allow only POST requests
+}));
+
 app.post('/api/submit', (req, res) => {
   const userDetails = req.body; 
   sendEmail(userDetails.__email, userDetails);
   res.status(200).json({ message: 'Form submitted successfully' });
 });
+
 function sendEmail(__email, userDetails) {
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -35,6 +43,7 @@ function sendEmail(__email, userDetails) {
     }
   });
 }
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
