aafe972d1a52ddb49a8b5c2c8f36963ab96a299b
Lakszy <lakshaykhattar0208@gmail.com>
2023-09-02 15:01:49 +0530

    second

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const port = 3000;
+const port = 'https://googleformfrontend.vercel.app/';
 
 app.use(bodyParser.json());
 
